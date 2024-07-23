df6b6e6dc55b9f428cf6c39996c48ecb3d7c78e6
Millian Lamiaux <millian.lamiaux@adeo.com>
2024-07-23 13:31:21 +0200

    Fixing detect-secrets call.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,10 @@ git config --global --add safe.directory /github/workspace
 
 if [ -n "${INPUT_BASELINE_PATH}" ]; then
     # When .secrets.baseline is provided, the file is only updated and not written to stdout
-    detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} --baseline ${INPUT_BASELINE_PATH} ${INPUT_WORKDIR}
+    /usr/local/bin/detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} --baseline ${INPUT_BASELINE_PATH} ${INPUT_WORKDIR}
     mv ${INPUT_BASELINE_PATH} /tmp/.secrets.baseline
 else
-    detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} ${INPUT_WORKDIR} > /tmp/.secrets.baseline
+    /usr/local/bin/detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} ${INPUT_WORKDIR} > /tmp/.secrets.baseline
 fi
 
 if [ "${INPUT_SKIP_AUDITED}" = "true" ]; then
