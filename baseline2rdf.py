e2646193d9679f75aa11d016d8cb6534c1befb06
Millian Lamiaux <millian.lamiaux@adeo.com>
2024-07-24 09:27:24 +0200

    Debugging AI inference process.

--- baseline2rdf.py
+++ baseline2rdf.py
@@ -26,52 +26,53 @@ def main(skip_audited: bool = False, verbose: bool = False):
     if not baseline['results']:
         baseline['results'] = {}
 
-    audit = None
     with open("/tmp/.secrets.audit", "r") as audit_file:
         audit = json.load(audit_file)
-        
-    if not audit['results']:
-        audit['results'] = {}
+    
+        print(audit)
 
-    # Use a pipeline as a high-level helper
-    pipe = pipeline("text-classification", model="adeoservicesai/BERT_secret_classification")
+        if not audit['results']:
+            audit['results'] = {}
 
-    results = {}
-    for detects in baseline['results'].values():
-        for item in detects:
-            if skip_audited and 'is_secret' in item and not item['is_secret']:
-                if verbose:
-                    print('Skipping verified secret in : %s' % item['filename'])
-            else:
-                for audit in audit['results'].values():
-                    if audit['filename'] == item['filename'] and item['line_number'] in audit['lines'].keys():
-                        if pipe(audit['lines'][item['line_number']])[0]['label'] == 'SECRET':
-                            key = '%s:%s' % (item['filename'], item['line_number'])
-                            if key in results:
-                                results[key]['message'] += '\n* ' + item['type']
-                            else:
-                                results[key] = {
-                                    'message': '\n* ' + item['type'],
-                                    'location': {
-                                        'path': item['filename'],
-                                        'range': {
-                                            'start': {
-                                                'line': item['line_number']
+        # Use a pipeline as a high-level helper
+        pipe = pipeline("text-classification", model="adeoservicesai/BERT_secret_classification")
+
+        results = {}
+        for detects in baseline['results'].values():
+            for item in detects:
+                if skip_audited and 'is_secret' in item and not item['is_secret']:
+                    if verbose:
+                        print('Skipping verified secret in : %s' % item['filename'])
+                else:
+                    for audit in audit['results'].values():
+                        if audit['filename'] == item['filename'] and item['line_number'] in audit['lines'].keys():
+                            if pipe(audit['lines'][item['line_number']])[0]['label'] == 'SECRET':
+                                key = '%s:%s' % (item['filename'], item['line_number'])
+                                if key in results:
+                                    results[key]['message'] += '\n* ' + item['type']
+                                else:
+                                    results[key] = {
+                                        'message': '\n* ' + item['type'],
+                                        'location': {
+                                            'path': item['filename'],
+                                            'range': {
+                                                'start': {
+                                                    'line': item['line_number']
+                                                }
                                             }
                                         }
                                     }
-                                }
 
-    for result in results.values():
-        rdjson['diagnostics'].append(result)
+        for result in results.values():
+            rdjson['diagnostics'].append(result)
 
-    try:
-        sys.stdout.write(json.dumps(rdjson, indent=2, ensure_ascii=False))
-        sys.stdout.write('\n')
-    except Exception as error:
-        sys.stderr.write('Error: %s\n' % error)
-        return 1
-    return 0
+        try:
+            sys.stdout.write(json.dumps(rdjson, indent=2, ensure_ascii=False))
+            sys.stdout.write('\n')
+        except Exception as error:
+            sys.stderr.write('Error: %s\n' % error)
+            return 1
+        return 0
 
 
 if __name__ == '__main__':
